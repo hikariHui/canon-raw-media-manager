ea085652951241452123da4bdd40d34578a83036
hikarihui <hikarihui@outlook.com>
2025-11-22 03:50:33 +0800

    feat: 新增底部操作提示

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import {
   NLayoutSider,
   NLayoutContent,
   NFlex,
+  NLayoutHeader,
+  NLayoutFooter,
 } from "naive-ui";
 
 import { rawDir, curCrmFile } from "./hooks/useRawFile";
@@ -14,6 +16,7 @@ import { proxyDir, curProxyFilePath } from "./hooks/useProxyFile";
 import VideoList from "./components/VideoList.vue";
 import PorxyPreview from "./components/PorxyPreview.vue";
 import VideoInfo from "./components/VideoInfo.vue";
+import OperationTips from "./components/OperationTips.vue";
 
 import { undo } from "./utils/oprationHistory";
 
@@ -43,22 +46,34 @@ window.addEventListener("keydown", (e) => {
 </script>
 
 <template>
-  <NLayout style="max-height: 100vh" position="absolute" has-sider>
-    <NLayoutSider>
-      <NButton @click="chooseDir('rawDir')">Choose Raw Dir</NButton>
-      <p>{{ rawDir }}</p>
-      <NButton @click="chooseDir('proxyDir')">Choose Proxy Dir</NButton>
-      <p>{{ proxyDir }}</p>
-      <VideoList />
-    </NLayoutSider>
-    <NLayout>
-      <NLayoutContent>
-        <PorxyPreview />
-        <NFlex>
-          <VideoInfo :video-path="curCrmFile" />
-          <VideoInfo :video-path="curProxyFilePath" />
-        </NFlex>
-      </NLayoutContent>
+  <div style="display: flex; flex-direction: column; height: 100vh">
+    <NLayoutHeader bordered>
+      <p>
+        <NButton @click="chooseDir('rawDir')">Choose Raw Dir</NButton
+        ><span>{{ rawDir }}</span>
+      </p>
+
+      <p>
+        <NButton @click="chooseDir('proxyDir')">Choose Proxy Dir</NButton
+        ><span>{{ proxyDir }}</span>
+      </p>
+    </NLayoutHeader>
+    <NLayout has-sider>
+      <NLayoutSider>
+        <VideoList />
+      </NLayoutSider>
+      <NLayout>
+        <NLayoutContent>
+          <PorxyPreview />
+          <NFlex>
+            <VideoInfo :video-path="curCrmFile" />
+            <VideoInfo :video-path="curProxyFilePath" />
+          </NFlex>
+        </NLayoutContent>
+      </NLayout>
     </NLayout>
-  </NLayout>
+    <NLayoutFooter bordered>
+      <OperationTips />
+    </NLayoutFooter>
+  </div>
 </template>
---
+++ src/components/OperationTips.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+
+const isFoucsed = ref(false);
+
+const onFocus = () => {
+  isFoucsed.value = true;
+};
+
+const onBlur = () => {
+  isFoucsed.value = false;
+};
+
+onMounted(() => {
+  window.addEventListener("focus", onFocus);
+  window.addEventListener("blur", onBlur);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("focus", onFocus);
+  window.removeEventListener("blur", onBlur);
+});
+</script>
+
+<template>
+  <div v-show="isFoucsed">
+    空格键：播放；左右箭头：快进/后退5秒；退格键：删除
+  </div>
+  <div v-show="!isFoucsed">应用未聚焦，快捷键不可用</div>
+</template>
